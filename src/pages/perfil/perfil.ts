269279538ce295db1fa02c163c98c7bcea8b9e8f
luisliev <luis.lievano94@gmail.com>
2018-07-10 14:45:32 -0500

    perfil page updated

--- src/pages/perfil/perfil.ts
+++ src/pages/perfil/perfil.ts
@@ -7,11 +7,23 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class PerfilPage {
 
+  options: Array<{my_option: string, icon_option: string}> = [];
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.options.push({my_option: "Editar perfil", icon_option: "brush"});
+    this.options.push({my_option: "Mis logros", icon_option: "star"});
+    this.options.push({my_option: "Favoritos", icon_option: "heart"});
+    this.options.push({my_option: "Pagos y facturas", icon_option: "cash"});
+    this.options.push({my_option: "Configuración", icon_option: "cog"});
+    this.options.push({my_option: "Cerrar sesión", icon_option: "log-out"});
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad PerfilPage');
+  }
+
+  optionSelected(opt) {
+    console.log("Option: " + opt);
   }
 
 }
