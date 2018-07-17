d6680e34713d4b12ae60ccc62885ab3fd324aa8a
luisliev <luis.lievano94@gmail.com>
2018-07-17 12:49:05 -0500

    cambios varios

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams, ToastController } from 'ionic-angular';
+import { NavController, NavParams, ToastController, App } from 'ionic-angular';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { TemplateCards } from '../cards01/cards01';
 
@@ -8,44 +8,56 @@ import { TemplateCards } from '../cards01/cards01';
   templateUrl: 'home.html',
 })
 export class HomePage {
-  constructor(public navCtrl: NavController, 
+  constructor(public navCtrl: NavController,
     public navParams: NavParams,
     private afAuth: AngularFireAuth,
-    private toast: ToastController) {
+    private toast: ToastController,
+    private app: App) {
   }
-/*
-  ionViewWillEnter(){
-   this.afAuth.authState.subscribe(data => {
-     if(data && data.email && data.uid) {
-     this.toast.create({
-       message: "Welcome to APP_NAME, ${data.email}", 
-       duration: 3000
-     }).present();
+  /*
+    ionViewWillEnter(){
+     this.afAuth.authState.subscribe(data => {
+       if(data && data.email && data.uid) {
+       this.toast.create({
+         message: "Welcome to APP_NAME, ${data.email}", 
+         duration: 3000
+       }).present();
+      }
+      else {
+        this.toast.create({
+          message: "Could not find authentication email", 
+          duration: 3000
+        }).present();
+      }
+     });
     }
-    else {
-      this.toast.create({
-        message: "Could not find authentication email", 
-        duration: 3000
-      }).present();
-    }
-   });
-  }
-*/
+  */
   //------------------------  Objetos para cada carta -------------------------------
-    Cat_Adventuras() {
-      this.navCtrl.push(TemplateCards, {
-        Adventuras: true
-      });
-    }
-    Cat_Gastronomia() {
-      this.navCtrl.push(TemplateCards, {
-        Gastronomia: true 
-      });
-    }
-    Cat_Souvenir() {
-      this.navCtrl.push(TemplateCards, {
-        Souvenir: true
-      })
-    }
-}
+  Cat_Adventuras() {
+    let nav = this.app.getRootNav();
+    nav.push(TemplateCards, {
+      animate: true,
+      animation: "bounce",
+      direction: "forward"
+    });
+  }
 
+  Cat_Gastronomia() {
+    let nav = this.app.getRootNav();
+    nav.push(TemplateCards, {
+      animate: true,
+      animation: "bounce",
+      direction: "forward"
+    });
+  }
+
+  Cat_Souvenir() {
+    let nav = this.app.getRootNav();
+    nav.push(TemplateCards, {
+      animate: true,
+      animation: "bounce",
+      direction: "forward"
+    });
+  }
+  
+}
