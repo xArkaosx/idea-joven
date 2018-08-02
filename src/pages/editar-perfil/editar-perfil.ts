ee686c46df39a3d11f8630bcda41f610df12cbc9
luisliev <luis.lievano94@gmail.com>
2018-08-02 13:32:34 -0500

    cambios varios

--- src/pages/editar-perfil/editar-perfil.ts
+++ src/pages/editar-perfil/editar-perfil.ts
@@ -28,9 +28,8 @@ export class EditarPerfilPage {
   createProfile() {
     this.afAuth.authState.take(1).subscribe(auth => {
       this.afDatabase.object(`profile/${auth.uid}`).set(this.profile)
-      .then(() => this.navCtrl.push(PerfilPage))
+      .then(() => this.popPage())
     })
   }
-
 
 }
